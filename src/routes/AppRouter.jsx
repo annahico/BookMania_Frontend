f4d5411a8f5731063067c95191aef2a7af2d236b
annahico <annahico@gmail.com>
2026-03-20 13:00:53 +0100

    feat: added navbar

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Layout from "../components/layout/Layout";
 
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
@@ -11,14 +12,12 @@ import MyReservationsPage from "../pages/reservations/MyReservationsPage";
 import AdminPage from "../pages/admin/AdminPage";
 
 const PrivateRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
-  if (loading) return <div>Cargando...</div>;
+  const { isAuthenticated } = useAuth();
   return isAuthenticated() ? children : <Navigate to="/login" />;
 };
 
 const AdminRoute = ({ children }) => {
-  const { isAdmin, loading } = useAuth();
-  if (loading) return <div>Cargando...</div>;
+  const { isAdmin } = useAuth();
   return isAdmin() ? children : <Navigate to="/" />;
 };
 
@@ -26,19 +25,19 @@ const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Públicas */}
+        {/* Rutas sin Navbar */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<BooksPage />} />
-        <Route path="/books/:id" element={<BookDetailPage />} />
 
-        {/* Privadas USER */}
-        <Route path="/my-loans" element={<PrivateRoute><MyLoansPage /></PrivateRoute>} />
-        <Route path="/my-fines" element={<PrivateRoute><MyFinesPage /></PrivateRoute>} />
-        <Route path="/my-reservations" element={<PrivateRoute><MyReservationsPage /></PrivateRoute>} />
-
-        {/* Privadas ADMIN */}
-        <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
+        {/* Rutas con Navbar */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<BooksPage />} />
+          <Route path="/books/:id" element={<BookDetailPage />} />
+          <Route path="/my-loans" element={<PrivateRoute><MyLoansPage /></PrivateRoute>} />
+          <Route path="/my-fines" element={<PrivateRoute><MyFinesPage /></PrivateRoute>} />
+          <Route path="/my-reservations" element={<PrivateRoute><MyReservationsPage /></PrivateRoute>} />
+          <Route path="/admin" element={<AdminRoute><AdminPage /></AdminRoute>} />
+        </Route>
 
         {/* Fallback */}
         <Route path="*" element={<Navigate to="/" />} />
